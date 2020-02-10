24ca04f5e25b688a32d7588f9c2d1dd255e9ad62
peter kuria <you@example.com>
2020-02-10 02:47:39 -0500

    added commenttest

--- insta/tests.py
+++ insta/tests.py
@@ -47,3 +47,28 @@ class ImageTest(TestCase):
         self.new_image.delete_image()
         image = Image.objects.all()
         self.assertTrue(len(image)==0)
+
+
+
+class CommentsTest(TestCase):
+
+    def setUp(self):
+        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
+        self.new_user.save()
+        self.new_image = Image(name='Moringa', image='moringa.jpg', caption='wonderful place to be', profile=self.new_user, like_add=0)
+        self.new_image.save()
+        self.new_comment = Comments(comment='This is a beautiful place',image=self.new_image,user=self.new_user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment, Comments))
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        comment = Comments.objects.all()
+        self.assertTrue(len(comment)>0)
+
+    def test_delete_comment(self):
+        self.new_comment.save_comment()
+        self.new_comment.delete_comment()
+        comment = Comments.objects.all()
+        self.assertTrue(len(comment)==0)        
