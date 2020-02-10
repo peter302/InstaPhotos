429cf7e88af9c7369e4729d1c1906aab5aa66702
peter kuria <you@example.com>
2020-02-10 05:46:26 -0500

    added

--- insta/admin.py
+++ insta/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, Profile, Comments
+
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comments)
