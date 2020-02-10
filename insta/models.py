46afcdb827566cd76ba861d8b751e305d0ffb322
peter kuria <you@example.com>
2020-02-10 01:59:08 -0500

    added methods associated with a profile model

--- insta/models.py
+++ insta/models.py
@@ -8,3 +8,26 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='images', default='profile.png')
     bio = HTMLField(blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+
+        def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_bio(cls,id, bio):
+        update_profile = cls.objects.filter(id = id).update(bio = bio)
+        return update_profile
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+    @classmethod
+    def search_user(cls,user):
+        return cls.objects.filter(user__username__icontains=user).all()
