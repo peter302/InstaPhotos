66742d0a8974ea1d76902a182985396ffa25d14c
peter kuria <you@example.com>
2020-02-10 02:15:03 -0500

    added imported various modules to views.py

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProfileForm,ImageForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Image, Profile,Comments,Likes
+from vote.managers import VotableManager
 
 # Create your views here.
