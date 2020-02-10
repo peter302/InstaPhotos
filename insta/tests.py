df5b6023a3a43d501c079fee394564d46d63f39d
peter kuria <you@example.com>
2020-02-10 02:46:05 -0500

    added profilestest

--- insta/tests.py
+++ insta/tests.py
@@ -2,5 +2,25 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-
+from .models import Profile, Image, User, Comments
 # Create your tests here.
+class ProfileTest(TestCase):
+
+    def setUp(self):
+        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
+        self.new_user.save()
+        self.new_profile = Profile(photo='image.png', bio='generous', user=self.new_user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile, Profile))
+
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)
