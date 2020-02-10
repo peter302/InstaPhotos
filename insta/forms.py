f5209fd3d4a710c3e5798cbe7f7bbf787b69f156
peter kuria <you@example.com>
2020-02-10 02:30:21 -0500

    added imageform class

--- insta/forms.py
+++ insta/forms.py
@@ -5,3 +5,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude=['user']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =[ 'profile_details', 'like_add', 'vote_score', 'num_vote_up', 'num_vote_down',]
