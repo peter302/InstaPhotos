b792ad6653463f55f4ce438bed72c62ae42af464
peter kuria <you@example.com>
2020-02-10 02:31:29 -0500

    added commentform  class

--- insta/forms.py
+++ insta/forms.py
@@ -10,3 +10,8 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude =[ 'profile_details', 'like_add', 'vote_score', 'num_vote_up', 'num_vote_down',]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image','user']
