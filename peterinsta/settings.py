0cf54bc0e687507b998e520ca0c995edc51a1b9d
peter kuria <you@example.com>
2020-02-10 01:54:48 -0500

    configured database

--- peterinsta/settings.py
+++ peterinsta/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,9 @@ WSGI_APPLICATION = 'peterinsta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'kuriainsta',
+        'USER': 'peter',
+        'PASSWORD': 'peterkuria'
     }
 }
 
