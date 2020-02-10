b228388db2fbf681651dd4a8115e56a82ad68635
peter kuria <you@example.com>
2020-02-10 02:02:27 -0500

    added database

--- peterinsta/settings.py
+++ peterinsta/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'peterinsta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kuriainsta',
         'USER': 'peter',
         'PASSWORD': 'peterkuria'
