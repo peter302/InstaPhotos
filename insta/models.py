37bdf07c78cbc19affea208cdb922844abcc5e4a
peter kuria <you@example.com>
2020-02-10 01:58:21 -0500

    added a profile model

--- insta/models.py
+++ insta/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='images', default='profile.png')
+    bio = HTMLField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
